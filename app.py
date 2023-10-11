6aa729890ad684f577ab343c938666b684541f8d
PedroAriasDev <pedroarias870@gmail.com>
2023-10-11 00:31:04 -0300

    Agrega funciones para calculo de entropia de fuente extendida de orden N
    Se agrega dos funciones para calcular la fuente extendida de orden N
      calculaDicProbabilidadesCombinadas: retorna un dicionario con las combinaciones de los bits para la fuente extendida junto a el valor de la probabilidad correspondiente.
      calculaEntropiaExtensionN: muestra por pantalla el diccionario de las  probabilidades combinadas y la entropia calculada a partir de estas mismas
    
    Cambios menores: en la funcion calculaVectorEstacionario, se elimina el if que se asegura que la fuente no sea nula ya que esto lo comprueba desde el main ahorrando las comprobaciones en los metodos

--- app.py
+++ app.py
@@ -70,25 +70,58 @@ class archivo:
         return p0 * math.log2(1 / p0) + p1 * math.log2(1 / p1)
 
     def calcularVectorEstacionario(self): 
-    
-        if(not nula()):
-            # Definir la matriz de probabilidad condicional (self.M)
-            matriz_condicional = np.array(self.M)
+        # Definir la matriz de probabilidad condicional (self.M)
+        matriz_condicional = np.array(self.M)
             
-            # Definir el vector de probabilidad inicial
-            vector_inicial = np.array([0.5, 0.5]) 
-            
-            # Calcular el vector estacionario
-            vector_estacionario = vector_inicial
-            nuevo_vector_estacionario = np.array([])
-            while (True):
-                nuevo_vector_estacionario = np.dot(vector_estacionario, matriz_condicional)
-                if np.equal(nuevo_vector_estacionario, vector_estacionario):
-                    # Si el vector no cambia termina el ciclo
-                    break
-                vector_estacionario = nuevo_vector_estacionario
+        # Definir el vector de probabilidad inicial
+        vector_inicial = np.array([0.5, 0.5]) 
+        
+        # Calcular el vector estacionario
+        vector_estacionario = vector_inicial
+        nuevo_vector_estacionario = np.array([])
+        while (True):
+            nuevo_vector_estacionario = np.dot(vector_estacionario, matriz_condicional)
+            if np.equal(nuevo_vector_estacionario, vector_estacionario):
+                # Si el vector no cambia termina el ciclo
+                break
+            vector_estacionario = nuevo_vector_estacionario
         
         return vector_estacionario
+    
+    
+    
+    def calculaDicProbabilidadesCombinadas(self, probabilidades, n):
+        if n == 1:
+            return {"0": probabilidades[0], "1": probabilidades[1]}
+        primera_parte = self.calculaDicProbabilidadesCombinadas(probabilidades, n - 1)
+        segunda_parte = primera_parte.copy()  # Copia el diccionario primera_parte
+        # Actualiza la primera parte con "0"
+        for key in primera_parte:
+            primera_parte[key + "0"] = primera_parte[key] * probabilidades[0]
+        # Actualiza la segunda parte con "1"
+        for key in segunda_parte:
+            segunda_parte[key + "1"] = segunda_parte[key] * probabilidades[1]
+        return {**primera_parte, **segunda_parte}
+
+
+    def calculaEntropiaExtensionN(self,n):
+        probabilidades=[self.M[0][0],self.M[1][1]]
+        entropia=0
+        #Se calcula el diccionario con las probabilidades combinadas segun el orden que se ingreso
+        dicProbabilidadesCombinadas=self.calculaDicProbabilidadesCombinadas(probabilidades,n)
+        print("El vector de probabilidades combinadas es:")
+        print(dicProbabilidadesCombinadas)
+        #Calculo de la entropia de la fuente extendida de orden n
+        for key in dicProbabilidadesCombinadas:
+            entropia+=dicProbabilidadesCombinadas[key]*math.log2(1/dicProbabilidadesCombinadas[key])
+        print("La entropia de la fuente extendida de orden: %d es:" %n)
+        print("Entropia: %f" % entropia)
+
+        
+
+
+        
+
 
 def convertirABinario(valor_byte):
     representacion_binaria = bin(valor_byte)[2:]
@@ -101,13 +134,22 @@ def convertirABinario(valor_byte):
 def main():
     #filename = sys.argv[1]
     filename = "tp1_sample0.bin"
-    n = 0
+    n=1
     # if(len(sys.argv) > 2):
     #     n = sys.argv[2]
+    # else
+    #     n=1
     #print("filename: " + filename + " n: " + n)
-    
     arch = archivo(filename, n)
     arch.abrirArchivo()
+    if(arch.nula()):
+        print("La fuente tiene simbolos estadisticamente independientes (es de memoria nula)")
+        print("La entropia de la fuente es: %d" %arch.entropiaMemoriaNula())
+        arch.calculaEntropiaExtensionN(n)
+    else:
+        print("La fuente tiene simbolos estadisticamente dependientes (es de memoria no nula)")
+        print("La entropia de la fuente es: %d" % arch.entropiaMemoriaNoNula())
+        
 
 if __name__ == "__main__":
     main()
