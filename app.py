64c75294c9acedfc0d2ec9c3dad93b27b9627c2e
facundodelga <128828081+facundodelga@users.noreply.github.com>
2023-10-12 17:05:17 -0300

    todo ok

--- app.py
+++ app.py
@@ -25,41 +25,45 @@ class archivo:
         self.M = [[0,0],[0,0]]
     
     def abrirArchivo(self):
-        with open(self.filename,"rb") as f:
-            file = f.read()
-            
-            for line in file:
-                binario = convertirABinario(line)
+        try:
+            with open(self.filename,"rb") as f:
+                file = f.read()
                 
-                bits = [int(bit) for bit in binario]
-                print(bits)
-                
-                anterior = bits[0]
-                bits.pop(0)
+                for line in file:
+                    binario = convertirABinario(line)
+                    
+                    bits = [int(bit) for bit in binario]
+                    # print(bits)
+                    
+                    anterior = bits[0]
+                    bits.pop(0)
 
-                for actual in bits:
-                    self.M[anterior][actual] += 1
-                    anterior = actual
+                    for actual in bits:
+                        self.M[anterior][actual] += 1
+                        anterior = actual
+                        
+            t0 = self.M[0][0] + self.M[1][0]  
+            t1 = self.M[1][1] + self.M[0][1]
+            
+            # Probabilidad desde el estado 0
+            self.M[0][0] /= t0  #p00
+            self.M[0][0] = round(self.M[0][0],2)
+            
+            self.M[1][0] /= t0  #p10
+            self.M[1][0] = round(self.M[1][0],2) 
+            
+            # Probabilidad desde el estado 1
+            self.M[0][1] /= t1  #p01
+            self.M[0][1] = round(self.M[0][1],2)
+            
+            self.M[1][1] /= t1  #p11
+            self.M[1][1] = round(self.M[1][1],2)
+            
+            return 0
+        except:
+            print("Error al abrir archivo")
+            return -1
         
-        t0 = self.M[0][0] + self.M[1][0]  
-        t1 = self.M[1][1] + self.M[0][1]
-        
-        # Probabilidad desde el estado 0
-        self.M[0][0] /= t0  #p00
-        self.M[0][0] = round(self.M[0][0],2)
-        
-        self.M[1][0] /= t0  #p10
-        self.M[1][0] = round(self.M[1][0],2) 
-        
-        # Probabilidad desde el estado 1
-        self.M[0][1] /= t1  #p01
-        self.M[0][1] = round(self.M[0][1],2)
-        
-        self.M[1][1] /= t1  #p11
-        self.M[1][1] = round(self.M[1][1],2)
-        
-        print(self.M)
-        return 0
     
     #b) Determinar si el contenido del archivo filename.bin proviene de una fuente binaria de memoria nula o no nula y calcular su entropía.
     def nula(self):
@@ -68,14 +72,14 @@ class archivo:
 
     def entropiaMemoriaNula(self): 
         #calculo de entropia para memoria nula
-        return self.M[0][0] * math.log2(1 / self.M[0][0]) + self.M[1][1] * math.log2 (1 / self.M[1][1])
+        return round(self.M[0][0] * math.log2(1 / self.M[0][0]) + self.M[1][1] * math.log2 (1 / self.M[1][1]),2)
 
     def  entropiaMemoriaNoNula(self): 
         p00 = self.M[0][0]
         p10 = self.M[0][1]
         p01 = self.M[1][0]
         p11 = self.M[1][1]
-
+        
         # if(p0 == 0 or p1 == 0):
         #     return 0
 
@@ -100,44 +104,46 @@ class archivo:
         
         return vector_estacionario
     
-    
-    
-    def calculaDicProbabilidadesCombinadas(self, probabilidades, n):
-        if n == 1:
-            return {"0": probabilidades[0], "1": probabilidades[1]}
-        
-        primera_parte = self.calculaDicProbabilidadesCombinadas(probabilidades, n - 1)
-        # segunda_parte = primera_parte.copy()  # Copia el diccionario primera_parte
-        
-        # Actualiza la primera parte con "0"
-        dict_aux = primera_parte.copy()
-        for key in dict_aux.keys():
-            primera_parte[key + "0"] = round(primera_parte[key] * probabilidades[0],2)
-        
-        for key in dict_aux.keys():
-            primera_parte[key + "1"] = round(primera_parte[key] * probabilidades[1],2)
             
-        # Actualiza la segunda parte con "1"
-        # dict_aux = segunda_parte.copy()
-        # for key in dict_aux.keys():
-        #     segunda_parte[key + "1"] = round(segunda_parte[key] * probabilidades[1],2)
-        
-        return primera_parte
+    def calculaDicProbabilidadesCombinadas(self, probabilidades, n):
+            #Caso de corte
+            if n == 1:
+                return {"0": probabilidades[0], "1": probabilidades[1]}
+            #Caso de no corte
+            fuenteAnterior = self.calculaDicProbabilidadesCombinadas(probabilidades, n - 1)
+            fuenteActual = {} #Inicializa diccionario vacio
+            
+            # Actualiza el diccionario sumandole un orden de fuente
+            for key in fuenteAnterior.keys():
+                fuenteActual[key + "0"] = round(fuenteAnterior[key] * probabilidades[0],2)
+                fuenteActual[key + "1"] = round(fuenteAnterior[key] * probabilidades[1],2)
+
+            return fuenteActual
 
 
     def calculaEntropiaExtensionN(self,n):
         probabilidades=[self.M[0][0],self.M[1][1]]
-        entropia = 0
+        entropia=0
+        
         #Se calcula el diccionario con las probabilidades combinadas segun el orden que se ingreso
         dicProbabilidadesCombinadas=self.calculaDicProbabilidadesCombinadas(probabilidades,n)
         print("El vector de probabilidades combinadas es:")
         print(dicProbabilidadesCombinadas)
+        
         #Calculo de la entropia de la fuente extendida de orden n
-        for key in dicProbabilidadesCombinadas.keys():
-            if(dicProbabilidadesCombinadas[key] != 0 and len(key) == n): 
-                entropia += dicProbabilidadesCombinadas[key] * math.log2(1 / dicProbabilidadesCombinadas[key])
+        for key in dicProbabilidadesCombinadas:
+            if(dicProbabilidadesCombinadas[key] != 0):
+                entropia += dicProbabilidadesCombinadas[key] * math.log2(1/dicProbabilidadesCombinadas[key])
+            
+        entropia = round(entropia,2)
+        
         print("La entropia de la fuente extendida de orden: %d es:" %n)
-        print("Entropia: %f" % entropia)
+        print("Entropia: " + str(entropia))
+        
+    def print_M(self):
+        print("Matriz de probabilidades condicionales")
+        print(self.M[0])
+        print(self.M[1])
 
 def convertirABinario(valor_byte):
     representacion_binaria = bin(valor_byte)[2:]
@@ -148,26 +154,27 @@ def convertirABinario(valor_byte):
     return representacion_binaria
 
 def main():
-    #filename = sys.argv[1]
-    filename = "tp1_sample3.bin"
-    n=3
-    # if(len(sys.argv) > 2):
-    #     n = sys.argv[2]
-    # else
-    #     n=1
-    #print("filename: " + filename + " n: " + n)
-    arch = archivo(filename, n)
-    arch.abrirArchivo()
-    print(str(arch.nula()))
-    if(arch.nula()):
-        print("La fuente tiene simbolos estadisticamente independientes (es de memoria nula)")
-        print("La entropia de la fuente es: " + str(arch.entropiaMemoriaNula()))
-        arch.calculaEntropiaExtensionN(n)
-    else:
-        print("La fuente tiene simbolos estadisticamente dependientes (es de memoria no nula)")
-        print("La entropia de la fuente es: " + str(arch.entropiaMemoriaNoNula()))
+    filename = sys.argv[1]
+    # filename = "tp1_sample0.bin"
     
-    # input(" fin ")
+    if(len(sys.argv) > 2):
+        n = sys.argv[2]
+    else:
+        n=1
+
+    arch = archivo(filename, n)
+
+    if(arch.abrirArchivo()):
+        arch.print_M()
+        
+        if(arch.nula()):
+            print("La fuente tiene simbolos estadisticamente independientes (es de memoria nula)")
+            print("La entropia de la fuente es: " + str(arch.entropiaMemoriaNula()))
+            arch.calculaEntropiaExtensionN(n)
+        else:
+            print("La fuente tiene simbolos estadisticamente dependientes (es de memoria no nula)")
+            print("La entropia de la fuente es: " + str(arch.entropiaMemoriaNoNula()))
+
 
 if __name__ == "__main__":
     main()
